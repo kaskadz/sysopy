e7c8a9f9586c04cd59454e24282bd332965b71b5
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-03-18 22:45:29 +0100

    Make some space

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -16,22 +16,28 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
     if (strcmp(command, "generate") == 0) {
         if (argc < 5) return fail_on_parameter();
+
         const char *file = argv[2];
         r = strtoul(argv[3], NULL, 10);
         b = strtoul(argv[4], NULL, 10);
+
         printf("%s: %s %zu %zu\n", command, file, r, b);
     } else if (strcmp(command, "sort") == 0) {
         if (argc < 5) return fail_on_parameter();
+
         const char *file = argv[2];
         r = strtoul(argv[3], NULL, 10);
         b = strtoul(argv[4], NULL, 10);
+
         printf("%s: %s %zu %zu\n", command, file, r, b);
     } else if (strcmp(command, "copy") == 0) {
         if (argc < 6) return fail_on_parameter();
+
         const char *file1 = argv[2];
         const char *file2 = argv[3];
         r = strtoul(argv[4], NULL, 10);
         b = strtoul(argv[5], NULL, 10);
+
         printf("%s: %s %s %zu %zu\n", command, file1, file2, r, b);
     } else {
         fprintf(stderr, "Unknown command: %s\n", command);
