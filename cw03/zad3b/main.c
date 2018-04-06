dfd9aa33ba98f33087051e27415adc47c3d172ba
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-04-06 15:12:21 +0200

    Structure reorganizaton

---
+++ cw03/zad3b/main.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+
+#include "rainbow.h"
+
+#define DELIM " \n"
+
+#define PREEXIT_FREE() {\
+free(args);             \
+if (line) free(line);   \
+fclose(file);           \
+}
+
+void show_start_msg(size_t line_no, char *task_name, rlim_t cpu_lim, rlim_t mem_lim) {
+    printf(
+            GRN
+            BOLD
+            ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+            YEL
+            "Starting execution of task from line %zu.\n"
+            "\"%s\"\n"
+            "With restrictions:\n"
+            "\t-> on cpu time: %zu [s]\n"
+            "\t-> on memory usage: %zu [MB]\n"
+            GRN
+            "----------------------------------------"
+            RESET
+            "\n",
+            line_no,
+            task_name,
+            cpu_lim,
+            mem_lim
+    );
+}
+
+void show_times(struct rusage *begin, struct rusage *end) {
+    time_t u_time =
+            (end->ru_utime.tv_sec - begin->ru_utime.tv_sec) * 1000000 + end->ru_utime.tv_usec - begin->ru_utime.tv_usec;
+    time_t s_time =
+            (end->ru_stime.tv_sec - begin->ru_stime.tv_sec) * 1000000 + end->ru_stime.tv_usec - begin->ru_stime.tv_usec;
+    printf(
+            RED
+            BOLD
+            "----------------------------------------\n"
+            YEL
+            "End of execution.\n"
+            "User time: %ld us\n"
+            "System time: %ld us\n"
+            RED
+            "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+            RESET
+            "\n",
+            u_time,
+            s_time
+    );
+}
+
+void limit_res(int type, rlim_t value) {
+    struct rlimit rlim;
+    rlim.rlim_cur = rlim.rlim_max = value;
+    if (setrlimit(type, &rlim) == -1) {
+        perror("Setrlimit error (CPU)");
+        exit(EXIT_FAILURE);
+    }
+}
+
+char **make_args(char *line, size_t *param_qtty, rlim_t *cpu_limit, rlim_t *mem_limit) {
+    char **args = NULL;
+    *param_qtty = 0;
+    char *token = strtok(line, DELIM);
+
+    args = realloc(args, sizeof(char *) * ++(*param_qtty));
+    if (args == 0) {
+        perror("Memory allocation error");
+        free(args);
+        return NULL;
+    } else {
+        args[(*param_qtty) - 1] = token;
+    }
+    token = strtok(NULL, DELIM);
+
+    if (token == NULL) {
+        free(args);
+        return NULL;
+    }
+    *cpu_limit = strtoul(token, NULL, 10);
+//    printf("cpu_limit: %zu\n", *cpu_limit);
+    token = strtok(NULL, DELIM);
+
+    if (token == NULL) {
+        free(args);
+        return NULL;
+    }
+    *mem_limit = strtoul(token, NULL, 10);
+//    printf("mem_limit: %zu\n", *mem_limit);
+    token = strtok(NULL, DELIM);
+
+    while (token) {
+        args = realloc(args, sizeof(char *) * ++(*param_qtty));
+        if (args == 0) {
+            perror("Memory reallocation error");
+            free(args);
+            return NULL;
+        } else {
+            args[(*param_qtty) - 1] = token;
+        }
+        token = strtok(NULL, DELIM);
+    }
+    args = realloc(args, sizeof(char *) * ++(*param_qtty));
+    args[(*param_qtty) - 1] = NULL;
+    return args;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Bad parameters\n");
+        exit(EXIT_FAILURE);
+    }
+    const char *filename = argv[1];
+
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    size_t line_no = 0;
+
+    file = fopen(filename, "r");
+    if (file == 0) {
+        perror("Fopen error: ");
+        exit(EXIT_FAILURE);
+    }
+    while ((read = getline(&line, &len, file)) != -1) {
+        ++line_no;
+        if (line[0] == '\n') continue;
+//        printf("%s", line);
+
+        size_t param_qtty;
+        rlim_t cpu_limit;
+        rlim_t mem_limit;
+        char **args = make_args(line, &param_qtty, &cpu_limit, &mem_limit);
+        if (args == NULL) {
+            //free..
+            if (line) free(line);
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+
+        show_start_msg(line_no, args[0], cpu_limit, mem_limit);
+
+        // timing
+        struct rusage ru_begin, ru_end;
+        getrusage(RUSAGE_CHILDREN, &ru_begin);
+
+        pid_t pid;
+        pid = fork();
+        if (pid == 0) {
+//            child's work
+            // set cpu limit
+            limit_res(RLIMIT_CPU, cpu_limit);
+            // set mem limit
+            limit_res(RLIMIT_AS, mem_limit * 1024 * 1024);
+            // execute
+            if (execvp(args[0], args) < 0) {
+                perror("Exec error");
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid == -1) {
+            perror("Fork error");
+            PREEXIT_FREE();
+            exit(EXIT_FAILURE);
+        } else {
+//            parent's work
+            pid_t end_id;
+            int status;
+            do {
+                end_id = waitpid(pid, &status, WNOHANG | WUNTRACED);
+                if (end_id == -1) {
+                    perror("Waitpid error");
+                    PREEXIT_FREE();
+                    exit(EXIT_FAILURE);
+                } else if (end_id == pid) {
+                    if (WIFEXITED(status)) {
+                        int exit_status = WEXITSTATUS(status);
+                        if (exit_status != 0) {
+                            fprintf(
+                                    stderr,
+                                    BCYN
+                                    BLK
+                                    "Execution of line %zu (%s) failed! Exit status: %d"
+                                    RESET
+                                    "\n",
+                                    line_no,
+                                    args[0],
+                                    exit_status
+                            );
+                            //free..
+                            PREEXIT_FREE();
+                            exit(EXIT_FAILURE);
+                        }
+                    } else if (WIFSIGNALED(status)) {
+                        fprintf(
+                                stderr,
+                                BMAG
+                                BLK
+                                "Child process ended because of an uncaught signal: %d"
+                                RESET
+                                "\n",
+                                WTERMSIG(status)
+                        );
+                        if (WCOREDUMP(status)) {
+                            fprintf(stderr, "Core dumped :c\n");
+                        }
+                    }
+                }
+            } while (end_id == 0);
+        }
+
+        // timing
+        getrusage(RUSAGE_CHILDREN, &ru_end);
+        show_times(&ru_begin, &ru_end);
+
+        free(args);
+    }
+    if (line) free(line);
+    fclose(file);
+
+    exit(EXIT_SUCCESS);
+}
+
---
+++ cw03/zad3b/rainbow.h
@@ -0,0 +1,35 @@
+//
+// Created by Kasper on 04/04/2018.
+//
+
+#ifndef CW03_RAINBOW_H
+#define CW03_RAINBOW_H
+//fg colors
+#define BLK     "\x1B[30m"
+#define RED     "\x1B[31m"
+#define GRN     "\x1B[32m"
+#define YEL     "\x1B[33m"
+#define BLU     "\x1B[34m"
+#define MAG     "\x1B[35m"
+#define CYN     "\x1B[36m"
+#define WHT     "\x1B[37m"
+
+//bg colors
+#define BBLK    "\x1B[40m"
+#define BRED    "\x1B[41m"
+#define BGRN    "\x1B[42m"
+#define BYEL    "\x1B[43m"
+#define BBLU    "\x1B[44m"
+#define BMAG    "\x1B[45m"
+#define BCYN    "\x1B[46m"
+#define BWHY    "\x1B[47m"
+
+//other
+#define BOLD    "\x1B[1m"
+#define UNDER   "\x1B[4m"
+#define BLINK   "\x1B[5m"
+#define REV     "\x1B[7m"
+#define HID     "\x1B[8m"
+#define RESET   "\x1B[0m"
+
+#endif //CW03_RAINBOW_H
