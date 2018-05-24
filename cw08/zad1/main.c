b81de5d04c36a89e97b8e3699a590931de0e072e
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-05-24 16:43:00 +0200

    Make it almos work
    Unfortunatelly some double freeing present :cry:

--- cw08/zad1/main.c
+++ cw08/zad1/main.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 
 #include "rainbow.h"
 
@@ -27,16 +28,34 @@
     {                                                                         \
         fprintf(stdout, "[" CYN "INFO" RESET "] " _MSG_ "\n", ##__VA_ARGS__); \
     }
+#define DEBUG(_MSG_, ...)                                                      \
+    {                                                                         \
+        fprintf(stdout, "[" YEL "INFO" RESET "] " _MSG_ "\n", ##__VA_ARGS__); \
+    }
 
 // - |GLOBAL VARIABLES| ------------------ //
 double **K;
-unsigned char **I;
-unsigned char **J;
-unsigned int c, w, h;
+int **I;
+int **J;
+int c, w, h;
+int thread_count;
+pthread_t *threads;
+int **arg;
 
 // - |FUNCTION DECLARATIONS| ------------- //
 void load_image(const char *filename);
+void load_filter(const char *filename);
 void init_image_buffers();
+void free_image_buffers();
+void init_filter_buffer();
+void free_filter_buffer();
+void init_threads_arrays();
+void free_threads_arrays();
+void save_image_buffer(const char *filename);
+void filter(int arg[2]);
+int max(int a, int b);
+int min(int a, int b);
+int get_index(int x, int bound);
 
 // - |MAIN| ------------------------------ //
 int main(int argc, char *argv[])
@@ -46,19 +65,86 @@ int main(int argc, char *argv[])
         ERR_EXIT("Wrong parameters given!");
     }
 
-    unsigned int thread_count = strtoul(argv[1], NULL, 10);
+    thread_count = strtoul(argv[1], NULL, 10);
     const char *input_image_filename = argv[2];
     const char *input_filter_filename = argv[3];
     const char *output_image_filename = argv[4];
 
+    init_threads_arrays();
+
     load_image(input_image_filename);
+    load_filter(input_filter_filename);
+
+    DEBUG("w = %d, h = %d, c = %d", w, h, c)
+
+    for (int i = 0; i < thread_count; i++)
+    {
+        arg[i][0] = (w / thread_count) * i;
+        arg[i][1] = w / thread_count;
+        DEBUG("Starting thread (%d, %d)", arg[i][0], arg[i][1])
+        pthread_create(&threads[i], NULL, (void *(*)(void *)) & filter, arg[i]);
+        // filter(arg);
+    }
+    arg[thread_count][0] = (w / thread_count) * thread_count;
+    arg[thread_count][1] = w % thread_count;
+    DEBUG("Starting thread (%d, %d)", arg[thread_count][0], arg[thread_count][1])
+    pthread_create(&threads[thread_count], NULL, (void *(*)(void *)) & filter, arg[thread_count]);
+    // filter(arg);
+
+    for (int i = 0; i < thread_count + 1; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    save_image_buffer(output_image_filename);
 
     free_image_buffers();
+    free_filter_buffer();
+    free_threads_arrays();
 
     exit(EXIT_SUCCESS);
 }
 
 // - |FUNCTION IMPLEMENTATIONS| ------------------------- //
+int get_index(int x, int bound)
+{
+    if (x < 0)
+        return 0;
+    if (x >= bound)
+        return bound - 1;
+    return x;
+}
+
+double sum(int x, int y)
+{
+    double result = 0;
+    for (int i = 0; i < c; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            int a = get_index(x - (int)ceil((double)c / 2.0) + i, w);
+            int b = get_index(y - (int)ceil((double)c / 2.0) + j, h);
+            result += (double)I[a][b] * K[i][j];
+        }
+    }
+    return result;
+}
+
+void filter(int *arg)
+{
+    int from = arg[0];
+    int len = arg[1];
+    DEBUG("Hello from thread %lu: (%d,%d)", pthread_self(), from, len)
+    INFO("Started filtering")
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            J[i + from][j] = round(sum(i + from, j));
+        }
+    }
+    INFO("Filtering finished")
+}
 
 void load_image(const char *filename)
 {
@@ -67,7 +153,7 @@ void load_image(const char *filename)
     if (file == NULL)
         PERR_EXIT("Error opening a file")
 
-    unsigned char maxval;
+    int maxval;
     fscanf(file, "P2 %d %d %d", &w, &h, &maxval);
     if (maxval != 255)
         ERR_EXIT("Wrong file format: Wrong max value!")
@@ -75,38 +161,108 @@ void load_image(const char *filename)
     init_image_buffers();
 
     // read image contents
-    for (size_t i = 0; i < w; i++)
+    for (int i = 0; i < w; i++)
     {
-        for (size_t j = 0; j < h; j++)
+        for (int j = 0; j < h; j++)
         {
-            unsigned char a;
-            if (fscanf(file, "%hhd", &a) == 1)
+            int a;
+            if (fscanf(file, "%d", &a) == 1)
                 I[i][j] = a;
             else
                 ERR_EXIT("Wrong file format: Not enough contents or wrong dimensions!")
         }
     }
 
-    INFO("Read the file")
+    INFO("Image loading finished");
 
     fclose(file);
 }
 
+void load_filter(const char *filename)
+{
+    INFO("Loading filter")
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+        PERR_EXIT("Error opening a filter file")
+
+    fscanf(file, "%d", &c);
+
+    init_filter_buffer();
+
+    // read image contents
+    for (int i = 0; i < c; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            double a;
+            if (fscanf(file, "%lf", &a) == 1)
+                K[i][j] = a;
+            else
+                ERR_EXIT("Wrong file format: Not enough contents or wrong matrix size!")
+        }
+    }
+
+    fclose(file);
+    INFO("Filter loading finished")
+}
+
+void init_threads_arrays()
+{
+    // initialize thread ids array
+    threads = calloc(thread_count + 1, sizeof(int));
+    // initialize thread args array
+    arg = calloc(thread_count + 1, sizeof(int*));
+    for(int i = 0; i < thread_count + 1; i++)
+    {
+        arg[i] = calloc(2, sizeof(int));
+    }
+}
+
+void free_threads_arrays()
+{
+    free(threads);
+    for(int i = 0; i < thread_count + 1; i++)
+    {
+        free(arg[i]);
+    }
+    free(arg);
+}
+
 void init_image_buffers()
 {
-    I = calloc(w, sizeof(unsigned char *));
-    J = calloc(w, sizeof(unsigned char *));
+    I = calloc(w, sizeof(int *));
+    J = calloc(w, sizeof(int *));
 
-    for (size_t i = 0; i < w; i++)
+    for (int i = 0; i < w; i++)
     {
-        I[i] = calloc(h, sizeof(unsigned char));
-        J[i] = calloc(h, sizeof(unsigned char));
+        I[i] = calloc(h, sizeof(int));
+        J[i] = calloc(h, sizeof(int));
     }
+}
+
+void init_filter_buffer()
+{
+    K = calloc(c, sizeof(double *));
+
+    for (int i = 0; i < c; i++)
+    {
+        K[i] = calloc(c, sizeof(double));
+    }
+}
+
+void free_filter_buffer()
+{
+    for (int i = 0; i < c; i++)
+    {
+        free(K[i]);
+    }
+
+    free(K);
 }
 
 void free_image_buffers()
 {
-    for (size_t i = 0; i < w; i++)
+    for (int i = 0; i < w; i++)
     {
         free(I[i]);
         free(J[i]);
@@ -114,4 +270,37 @@ void free_image_buffers()
 
     free(I);
     free(J);
+}
+
+void save_image_buffer(const char *filename)
+{
+    INFO("Saving output image to file")
+    FILE *file = fopen(filename, "w+");
+    if (file == NULL)
+        PERR_EXIT("Error opening output file")
+
+    fprintf(file, "P2\n%d %d\n%d\n", w, h, 255);
+
+    // read image contents
+    for (int i = 0; i < w; i++)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            fprintf(file, "%d ", J[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+    INFO("Saving output image finished")
+}
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int min(int a, int b)
+{
+    return (a < b) ? a : b;
 }
